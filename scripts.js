38262dc007b6474e6ba4cfbf639f8651da5d3803
Hector Jarquin <hectorjarquin@outlook.com>
2015-12-31 13:25:24 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+(function (document, window) {
+	
+	document.addEventListener('AstroWP-render', function (e) {
+		
+		$(window).ready(function() {
+			$('body').fadeIn(500);
+		});
+		
+		$('.bxslider').bxSlider({
+		  minSlides: 2,
+		  maxSlides: 7,
+		  slideWidth: 220,
+		  slideMargin: 10
+		});
+		
+    });
+	
+})(document, window);
